cb423c84c5754090b691c5e07b8fac965745739e
putricarel <149338743+putricarel@users.noreply.github.com>
2024-12-13 13:19:33 +0700

    home.py

--- Tabs/home.py
+++ Tabs/home.py
@@ -7,29 +7,28 @@ import pandas as pd
 def generate_plots(df):
     # Plot histogram
     st.subheader("Histogram Distribusi Fitur")
-    plt.figure(figsize=(12, 6))
     for column in df.columns:
         if df[column].dtype != 'O':  # Plot hanya untuk fitur numerik
+            plt.figure(figsize=(12, 6))
             sns.histplot(df[column], bins=20, kde=True)
-            st.pyplot()
+            st.pyplot(plt)
+            plt.clf()  # Clear the figure after plotting
             st.write(f"### Distribusi dari {column}")
 
     # Plot correlation heatmap
     st.subheader("Heatmap Korelasi Fitur")
     plt.figure(figsize=(10, 8))
     sns.heatmap(df.corr(), annot=True, cmap='coolwarm', fmt='.2f', linewidths=.5)
-    st.pyplot()
+    st.pyplot(plt)
+    plt.clf()  # Clear the figure after plotting
 
 # Load dataset
-st.set_option('deprecation.showPyplotGlobalUse', False)
-
 def load_data():
     # Load dataset sesuai kebutuhan
     df = pd.read_csv('Penyakit_Paru.csv')
     return df
 
 def app():
-
     # Judul halaman aplikasi
     st.title("APLIKASI PREDIKSI TERKENA PENYAKIT PARU-PARU")
     
@@ -40,7 +39,15 @@ def app():
     st.image('paru.jpg', use_column_width=True)
 
     # Tampilkan keterangan
-    st.write("Penyakit paru-paru adalah salah satu penyebab kematian tertinggi di dunia. Penyakit ini dapat disebabkan oleh berbagai faktor. Aplikasi ini dapat digunakan untuk memperkirakan risiko seseorang terkena penyakit paru-paru berdasarkan faktor-faktor risiko yang dapat diukur. aplikasi ini sudah melakukan riset kepada 30rb orang yang diliat dari beberapa faktor yaitu : Usia, Jenis_Kelamin, Merokok, Bekerja, Rumah_Tangga, Aktivitas_Begadang, Aktivitas_Olahraga, Asuransi, Penyakit_Bawaan,Hasil.")
+    st.write("Penyakit paru-paru adalah salah satu penyebab kematian tertinggi di dunia. "
+             "Penyakit ini dapat disebabkan oleh berbagai faktor. Aplikasi ini dapat digunakan "
+             "untuk memperkirakan risiko seseorang terkena penyakit paru-paru berdasarkan faktor-faktor "
+             "risiko yang dapat diukur. Aplikasi ini sudah melakukan riset kepada 30 ribu orang "
+             "yang dilihat dari beberapa faktor yaitu: Usia, Jenis_Kelamin, Merokok, Bekerja, "
+             "Rumah_Tangga, Aktivitas_Begadang, Aktivitas_Olahraga, Asuransi, Penyakit_Bawaan, Hasil.")
              
     # Tampilkan grafik
     generate_plots(df)
+
+if __name__ == "__main__":
+    app()
